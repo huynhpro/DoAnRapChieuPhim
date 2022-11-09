60314c196ad27ae836754f0b29a99115475871cb
huynhpro <huynh13nd@gmail.com>
2022-11-09 20:46:21 +0700

    first

---
+++ BUS/NhanVienBUS.cs
@@ -0,0 +1,73 @@
+﻿using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class NhanVienBUS
+    {
+        public List<NhanVien> getDSNhanVien()
+        {
+            List<NhanVien> listnv = new List<NhanVien>();
+            NhanVienDAL login = new NhanVienDAL();
+            listnv = login.getDsNhanVien();
+            return listnv;
+        }
+        public bool checkDs()
+        {
+            if (getDSNhanVien().Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public string getUser(string ten)
+        {
+            List<NhanVien> listnv = new List<NhanVien>();
+            NhanVienDAL login = new NhanVienDAL();
+            listnv = login.getDsNhanVien();
+            foreach (var item in listnv)
+            {
+                if (ten == item.TenNhanVien && item.UserName != null)
+                {
+                    return item.UserName;
+                }
+            }
+            return "Không có username";
+        }
+        public bool checkLogin(string username, string password)
+        {
+            List<NhanVien> listnv = new List<NhanVien>();
+            NhanVienDAL login = new NhanVienDAL();
+            listnv = login.getDsNhanVien();
+            foreach (var item in listnv)
+            {
+                if (item.UserName == username && item.PassWord == password && item.IsDelete == false && item.IsActive == true)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public bool checkLoginAdmin(string username, string password)
+        {
+            List<NhanVien> listnv = new List<NhanVien>();
+            NhanVienDAL login = new NhanVienDAL();
+            listnv = login.getDsNhanVien();
+            foreach (var item in listnv)
+            {
+                if (item.UserName == username && item.PassWord == password && item.IsDelete == false && item.IsActive == true && item.IsAdmin == true)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
---
+++ DAL/NhanVienDAL.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class NhanVienDAL
+    {
+        public List<NhanVien> getDsNhanVien()
+        {
+            List<NhanVien> listNV = new List<NhanVien>();
+            using (var db = new RapChieuPhimEntities())
+            {
+                var tb = from nv in db.NhanViens
+                         select nv;
+                foreach (var item in tb)
+                {
+                    NhanVien temp = new NhanVien();
+                    temp.IDNhanVien = item.IDNhanVien;
+                    temp.TenNhanVien = item.TenNhanVien;
+                    temp.NgaySinh = item.NgaySinh;
+                    temp.SDT = item.SDT;
+                    temp.DiaChi = item.DiaChi;
+                    temp.Email = item.Email;
+                    temp.UserName = item.UserName;
+                    temp.PassWord = item.PassWord;
+                    temp.IsAdmin = item.IsAdmin;
+                    temp.IsActive = item.IsActive;
+                    temp.IsDelete = item.IsDelete;
+                    temp.IsReceipt = item.IsReceipt;
+                    listNV.Add(temp);
+                }
+            }
+            return listNV;
+        }
+    }
+}
